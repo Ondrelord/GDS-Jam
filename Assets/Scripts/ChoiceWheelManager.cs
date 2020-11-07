e83bedc13c78b540afefcfb7f529baf92436bba2
Ondrej Čakloš <caklos.ondrej@gmail.com>
2020-11-07 10:07:15 +0100

    choice wheel mockup

---
+++ Assets/Scripts/ChoiceWheelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceWheelManager : MonoBehaviour
+{
+    [SerializeField] GameObject choiceWheel;
+    [SerializeField] Button rumourButton;
+    [SerializeField] Button bribeButton;
+    [SerializeField] Button shopButton;
+
+    IChoicesForWheel currentChoices;
+
+    public void OpenWheel(IChoicesForWheel choices)
+    {
+        choiceWheel.SetActive(true);
+        currentChoices = choices;
+
+        rumourButton.onClick.AddListener(choices.GetRumour);
+        bribeButton.onClick.AddListener(choices.GetBribe);
+        shopButton.onClick.AddListener(choices.GetShop);
+    }
+
+    public void CloseWheel()
+    {
+        choiceWheel.SetActive(false);
+
+        if (currentChoices != null)
+        {
+            rumourButton.onClick.RemoveListener(currentChoices.GetRumour);
+            bribeButton.onClick.RemoveListener(currentChoices.GetBribe);
+            shopButton.onClick.RemoveListener(currentChoices.GetShop);
+        }
+    }
+}
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+
+public interface IChoicesForWheel
+{
+    void GetRumour();
+    void GetBribe();
+    void GetShop();
+
+}
+
+
+public class NPC : MonoBehaviour, IChoicesForWheel
 {
     [SerializeField] string npcName;
     [SerializeField] int bribe_price;
 
-    [SerializeField] DialogueScriptableObject Rumor;
-    [SerializeField] DialogueScriptableObject Bribe;
-    [SerializeField] DialogueScriptableObject Shop;
+    [SerializeField] DialogueScriptableObject rumour;
+    [SerializeField] DialogueScriptableObject bribe;
+    [SerializeField] DialogueScriptableObject shop;
 
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void TriggerConversation(DialogueScriptableObject dialogue)
+    {
+        FindObjectOfType<DialogueManager>().StartConversation(dialogue);
+
+        if (dialogue.HaveFollowupDialogue())
+            dialogue = dialogue.GetFollowupDialogue();
+    }
+
+    private void OnMouseDown()
+    {
+        FindObjectOfType<ChoiceWheelManager>().OpenWheel(this);
+    }
+
+    public void GetRumour()
+    {
+        TriggerConversation(rumour);
+    }
+
+    public void GetBribe()
+    {
+        TriggerConversation(bribe);
+    }
+
+    public void GetShop()
+    {
+        TriggerConversation(shop);
     }
 }
